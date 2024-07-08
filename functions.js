a7b9f62649ad761f26d1b852e9699c527d5fc3e0
lkimwilu <lkimwilu@gmail.com>
2024-07-08 11:21:02 +0300

    modified html

---
+++ functions.js
@@ -0,0 +1,23 @@
+// function displayName() {
+//     name ="John Doe";
+
+//     return name;
+// }
+// console.log(displayName());
+
+// function displaySum() {
+//     var a = 10;
+//     var b = 20;
+
+//     return (sum = a + b);
+// }
+
+// console.log(displaySum());
+
+function displayProduct() {
+    var num1 =20;
+    var num2 =30;
+
+    return (Product = num1 * num2);
+}
+console.log(displayProduct());
---
+++ variables.js
@@ -0,0 +1,8 @@
+namesInClass = ["Antony", "Constance", 21,true, null, "Jasmine", "Katie"];
+
+ErastusAge =21;
+isKenyan = true;
+
+console.log(typeof namesInClass);
+
+console.log(typeof ErastusAge);
