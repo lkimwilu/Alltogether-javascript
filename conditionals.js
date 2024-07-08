c1fdd46e88d417759b47bc0f3624332f733a80c7
lkimwilu <lkimwilu@gmail.com>
2024-07-08 08:38:34 +0300

    create conditionals

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+// let age = 18;
+
+// if  (age >= 18) {
+//     console.log("You can get a driver's license");
+// } 
+
+// else {
+//     console.log("You can get a license");
+// }
+
+// let time = 11;
+
+// if (time < 12) {
+//     console.log("Good morning");
+// }
+
+let score = ;
+
+if (score =90) {
+    console.log(You got )
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+let name = "Lkim";
+let age = 28;
+let isStudent = false; 
+console.log(name);
+console.log(age);
+
+let sum =a=+b;
+let difference = a - b;
+let product = a * b;
+let divide = a / b;
+
+console.log("The result of add a and b is:"+sum);
+console.log("The result of minus a and b is:"+difference);
