0512170fa8037cea2f4c13b7753f43bf71bbeca6
lkimwilu <lkimwilu@gmail.com>
2024-07-08 12:32:12 +0300

    createsalltogether

---
+++ scope.js
@@ -0,0 +1,11 @@
+let globalVar = "I'm a global variable";
+
+function demoScope() {
+    let localVar = "I'm a local variable";
+    // console.log(globalVar);
+    // console.log(localVar);
+
+}
+ demoScope();
+//  console.log(globalVar);
+ console.log(localVar);
